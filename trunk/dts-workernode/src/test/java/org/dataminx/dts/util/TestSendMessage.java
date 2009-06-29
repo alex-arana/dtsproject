dfd66eac419a0d8a21a94cb68afb6275f6358b21
alex.arana <alex.arana@89d2972e-3acd-11de-8ef0-0f53c1b7e458>
2009-06-29 01:38:25 +0000

    Modified TestSendMessage so it is restartable.

--- trunk/dts-workernode/src/test/java/org/dataminx/dts/util/TestSendMessage.java
+++ trunk/dts-workernode/src/test/java/org/dataminx/dts/util/TestSendMessage.java
@@ -5,16 +5,37 @@
  */
 package org.dataminx.dts.util;
 
+import static org.apache.commons.lang.SystemUtils.JAVA_IO_TMPDIR;
+import static org.apache.commons.lang.SystemUtils.LINE_SEPARATOR;
+
 import java.io.File;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+import java.util.UUID;
+import javax.xml.transform.dom.DOMResult;
+import org.dataminx.dts.common.XmlUtils;
 import org.dataminx.dts.jms.JobSubmitQueueSender;
+import org.dataminx.schemas.dts._2009._05.dts.CreationFlagEnumeration;
+import org.dataminx.schemas.dts._2009._05.dts.DataTransferType;
+import org.dataminx.schemas.dts._2009._05.dts.JobDefinitionType;
+import org.dataminx.schemas.dts._2009._05.dts.JobDescriptionType;
+import org.dataminx.schemas.dts._2009._05.dts.JobIdentificationType;
+import org.dataminx.schemas.dts._2009._05.dts.ObjectFactory;
+import org.dataminx.schemas.dts._2009._05.dts.SourceTargetType;
+import org.dataminx.schemas.dts._2009._05.dts.SubmitJobRequest;
+import org.dataminx.schemas.dts._2009._05.dts.TransferRequirementsType;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.DOMOutputter;
+import org.jdom.xpath.XPath;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
 import org.w3c.dom.Document;
 
 /**
@@ -28,15 +49,80 @@ public class TestSendMessage {
     @Autowired
     private JobSubmitQueueSender mJmsQueueSender;
 
+    @Autowired
+    private Jaxb2Marshaller mMarshaller;
+
     @Test
-    public void submitDtsJob() throws Exception {
-        final File file = new ClassPathResource("minx-dts.xml").getFile();
-        final DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-        docFactory.setNamespaceAware(true);
-        final DocumentBuilder builder = docFactory.newDocumentBuilder();
-        final Document dtsJob = builder.parse(file);
+    public void submitDtsJobAsDocument() throws Exception {
+        File file = new ClassPathResource("minx-dts.xml").getFile();
+        final SAXBuilder builder = new SAXBuilder();
+        final org.jdom.Document dtsJob = builder.build(file);
+
+        XPath xpathEvaluator = XPath.newInstance(
+            "/pre:submitJobRequest/pre:JobDefinition/pre:JobDescription/pre:JobIdentification/pre:JobName");
+        xpathEvaluator.addNamespace("pre", "http://schemas.dataminx.org/dts/2009/05/dts");
+        Element node = (Element) xpathEvaluator.selectSingleNode(dtsJob);
+        Assert.notNull(node);
+        node.setText(generateNewJobId());
+
+        xpathEvaluator = XPath.newInstance(
+            "/minx:submitJobRequest/minx:JobDefinition/minx:JobDescription/minx:DataTransfer/minx:Target/minx:URI");
+        xpathEvaluator.addNamespace("minx", "http://schemas.dataminx.org/dts/2009/05/dts");
+        node = (Element) xpathEvaluator.selectSingleNode(dtsJob);
+        Assert.notNull(node);
+        file = new File(JAVA_IO_TMPDIR, "DataMINX_Logo.jpg");
+        node.setText(file.getAbsolutePath());
 
         //logger.info(client.submitJob(dtsJob));
-        mJmsQueueSender.doSend(dtsJob);
+        final DOMOutputter outputter = new DOMOutputter();
+        mJmsQueueSender.doSend(outputter.output(dtsJob));
+    }
+
+    @Test
+    public void submitDtsJobAsText() throws Exception {
+        final ObjectFactory factory = new ObjectFactory();
+
+        final SourceTargetType source = factory.createSourceTargetType();
+        source.setURI("http://wiki.arcs.org.au/pub/DataMINX/DataMINX/minnie_on_the_run.jpg");
+
+        final SourceTargetType target = factory.createSourceTargetType();
+        final File file = new File(JAVA_IO_TMPDIR, "DataMINX_Logo2.jpg");
+        target.setURI(file.getAbsolutePath());
+
+        final TransferRequirementsType transferRequirements = factory.createTransferRequirementsType();
+        transferRequirements.setMaxAttempts(0L);
+        transferRequirements.setCreationFlag(CreationFlagEnumeration.OVERWRITE);
+
+        final DataTransferType dataTransfer = factory.createDataTransferType();
+        dataTransfer.setTransferRequirements(transferRequirements);
+        dataTransfer.setSource(source);
+        dataTransfer.setTarget(target);
+
+        final JobIdentificationType jobIdentification = factory.createJobIdentificationType();
+        jobIdentification.setJobName(generateNewJobId());
+        jobIdentification.setDescription("Copies the DataMINX Logo from a HTTP source to a local folder");
+
+        final JobDescriptionType jobDescription = factory.createJobDescriptionType();
+        jobDescription.setJobIdentification(jobIdentification);
+        jobDescription.getDataTransfer().add(dataTransfer);
+
+        final JobDefinitionType jobDefinition = factory.createJobDefinitionType();
+        jobDefinition.setJobDescription(jobDescription);
+
+        final SubmitJobRequest submitJobRequest = factory.createSubmitJobRequest();
+        submitJobRequest.setJobDefinition(jobDefinition);
+
+        final DOMResult result = new DOMResult();
+        mMarshaller.marshal(submitJobRequest, result);
+        final Document document = (Document) result.getNode();
+
+        final Logger logger = LoggerFactory.getLogger(getClass());
+        final String dtsJobRequest = XmlUtils.documentToString(document);
+        logger.debug("DTS Job Request:" + LINE_SEPARATOR + dtsJobRequest);
+        mJmsQueueSender.doSend(dtsJobRequest);
+    }
+
+    private String generateNewJobId() {
+        return "DTSJob_" + UUID.randomUUID();
     }
 }
