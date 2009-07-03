4dae6aa93aa734d421e4f79026a31ad1b15e5766
gerson721 <gerson721@89d2972e-3acd-11de-8ef0-0f53c1b7e458>
2009-07-03 06:03:55 +0000

    - moved the content validation in WS in a separate class - cleaned up the interface and implementation of the DTSService - added an empty class which will later on handle job event updates from the worker node

---
+++ trunk/dts-ws/src/main/java/org/dataminx/dts/common/util/JobContentValidator.java
@@ -0,0 +1,44 @@
+package org.dataminx.dts.common.util;
+
+import org.dataminx.dts.ws.DtsJobDefinitionException;
+import org.dataminx.schemas.dts._2009._05.dts.DataTransferType;
+import org.dataminx.schemas.dts._2009._05.dts.JobDefinitionType;
+
+/**
+ * A validator of the contents of the Job definition document. Having a valid XML document
+ * doesn't necessarily mean the file has all the necessary documents for a job to successfully run.
+ * This class will check for the missing information that the user needs to provide and throw an
+ * Exception with the list of missing information as the message of the Exception.
+ *
+ * @author Gerson Galang
+ */
+public class JobContentValidator {
+
+    /**
+     * Checks for the validity of the contents of the Job Definition document.
+     *
+     * @param job the job definition
+     * @throws DtsJobDefinitionException if at least one of the elements in the Job definition document is invalid
+     */
+    public void validate(JobDefinitionType job) throws DtsJobDefinitionException {
+        StringBuffer errorMessages = new StringBuffer();
+
+        if (job.getJobDescription().getJobIdentification().getJobName().trim().equals("")) {
+            errorMessages.append("  Empty job name.\n");
+        }
+        // TODO: think of a cleaner way of returning back an error message for a number of
+        // missing source/target URIs
+        for (DataTransferType transfer : job.getJobDescription().getDataTransfer()) {
+            if (transfer.getSource().getURI().trim().equals("")) {
+                errorMessages.append("  Empty SourceURI.\n");
+            }
+            if (transfer.getTarget().getURI().trim().equals("")) {
+                errorMessages.append("  Empty TargetURI.\n");
+            }
+        }
+        if (errorMessages.length() > 0) {
+            throw new DtsJobDefinitionException("Invalid request.\n" + errorMessages);
+        }
+    }
+
+}
---
+++ trunk/dts-ws/src/main/java/org/dataminx/dts/jms/DtsJobEventUpdateHandler.java
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+package org.dataminx.dts.jms;
+
+
+/**
+ * The Handler for all the Job Event Update messages coming from the Worker Node
+ *
+ * @author Gerson Galang
+ *
+ */
+public class DtsJobEventUpdateHandler {
+}
