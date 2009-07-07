70c83de0ed06ddc20953030f0be3b2017d57604c
gerson721 <gerson721@89d2972e-3acd-11de-8ef0-0f53c1b7e458>
2009-07-07 23:26:17 +0000

    working version of the integrated WS and WN. the WS now updates the entry on the database once it gets an updateJobUpdate event from the WN. the problem with WS receiving a number of unrelated notifications from the WN was a result of a cached updateJobEvent messages on activeMQ when no handler was written for it at the time it was first being tested.

--- dts-ws/src/main/java/org/dataminx/dts/jms/DtsJobEventUpdateHandler.java
+++ dts-ws/src/main/java/org/dataminx/dts/jms/DtsJobEventUpdateHandler.java
@@ -1,11 +1,13 @@
-/**
- *
- */
 package org.dataminx.dts.jms;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.dataminx.dts.domain.model.Job;
+import org.dataminx.dts.domain.model.JobStatus;
+import org.dataminx.dts.domain.repo.JobDao;
+import org.dataminx.schemas.dts._2009._05.dts.JobEventDetailType;
 import org.dataminx.schemas.dts._2009._05.dts.JobEventUpdateRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.core.Message;
 
 
@@ -20,9 +22,44 @@ public class DtsJobEventUpdateHandler {
     /** The logger. */
     private static final Log LOGGER = LogFactory.getLog(DtsJobEventUpdateHandler.class);
 
+    /** The job repository for this DTS implementation. */
+    @Autowired
+    private JobDao mJobRepository;
+
+    /**
+     * Updates the job entity based on the details provided by the worker node
+     *
+     * @param message the jobEventUpdateRequest message
+     */
     public void updateJob(Message<JobEventUpdateRequest> message) {
         JobEventUpdateRequest request = message.getPayload();
-        // TODO: do something interesting here..
 
+        // TODO: probably need to look at making the resourcekey to WN job ID mapping clear (same names?)
+        // later on
+
+        // TODO: need to sync this with whatever is supported by the WN
+
+        // get the details of the job entry to be updated
+        String updatedJobResourceKey = request.getJobId();
+        JobEventDetailType updatedJobDetail = request.getJobEventDetail();
+
+        // job to update
+        Job job = mJobRepository.findByResourceKey(updatedJobResourceKey);
+
+        switch(updatedJobDetail.getStatus()) {
+            case TRANSFERRING:
+                job.setWorkerNodeHost(updatedJobDetail.getWorkerNodeHost());
+                job.setActiveTime(updatedJobDetail.getActiveTime().toGregorianCalendar().getTime());
+                job.setStatus(JobStatus.TRANSFERRING);
+                break;
+            case DONE:
+                job.setFinishedFlag(updatedJobDetail.isFinishedFlag());
+                job.setWorkerTerminatedTime(updatedJobDetail.getWorkerTerminatedTime().toGregorianCalendar().getTime());
+                job.setStatus(JobStatus.DONE);
+                break;
+            default:
+                break;
+        }
+        mJobRepository.saveOrUpdate(job);
     }
 }
