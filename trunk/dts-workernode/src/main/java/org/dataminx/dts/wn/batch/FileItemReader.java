242b43167b5452cc600dcc7543bda8e4eb2d7729
alex.arana <alex.arana@89d2972e-3acd-11de-8ef0-0f53c1b7e458>
2009-07-17 07:20:31 +0000

    Refactoring of all classes in the DTS WorkerNode module to use base package org.dataminx.dts.wn

---
+++ trunk/dts-workernode/src/main/java/org/dataminx/dts/wn/batch/FileItemReader.java
@@ -0,0 +1,73 @@
+/**
+ * Intersect Pty Ltd (c) 2009
+ *
+ * License: To Be Announced
+ */
+package org.dataminx.dts.wn.batch;
+
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import org.apache.commons.vfs.FileObject;
+import org.springframework.batch.item.support.AbstractItemCountingItemStreamItemReader;
+import org.springframework.util.Assert;
+
+
+/**
+ * Restartable {@link org.springframework.batch.item.ItemReader} that reads bytes from input
+ * VFS file object.
+ *
+ * @author Alex Arana
+ */
+public class FileItemReader extends AbstractItemCountingItemStreamItemReader<ByteBuffer> {
+    /** A reference to the underlying VFS file object. */
+    private final FileObject mFileObject;
+
+    /** A channel to read bytes from the input stream. */
+    private ReadableByteChannel mInputChannel;
+
+    /** The number of bytes copied in between calls to {@link #read()}. */
+    private int mBytesCopied;
+
+    /**
+     * Creates a new instance of {@link FileItemReader} using the given VFS file object.
+     *
+     * @param fileObject A VFS file object
+     */
+    public FileItemReader(final FileObject fileObject) {
+        mFileObject = fileObject;
+        setName(mFileObject.getName().getFriendlyURI());
+    }
+
+    @Override
+    protected void doClose() throws Exception {
+        mBytesCopied = 0;
+        mInputChannel.close();
+    }
+
+    @Override
+    protected void doOpen() throws Exception {
+        Assert.notNull(mFileObject, "File source cannot be null");
+        final InputStream in = mFileObject.getContent().getInputStream();
+        mInputChannel = Channels.newChannel(in);
+        //TODO handle bytes to skip in restarted scenario
+    }
+
+    @Override
+    protected ByteBuffer doRead() throws Exception {
+        //TODO implement a buffering strategy
+        final ByteBuffer buffer = ByteBuffer.allocateDirect(16 * 1024);
+
+        final int bytesRead = mInputChannel.read(buffer);
+        if (bytesRead == -1) {
+            // we've reached the end of the input file
+            return null;
+        }
+        mBytesCopied += bytesRead;
+
+        // prepare the buffer to be drained
+        buffer.flip();
+        return buffer;
+    }
+}
---
+++ trunk/dts-workernode/src/main/java/org/dataminx/dts/wn/common/util/InputDataUtils.java
@@ -0,0 +1,48 @@
+/**
+ * Intersect Pty Ltd (c) 2009
+ *
+ * License: To Be Announced
+ */
+package org.dataminx.dts.wn.common.util;
+
+import java.util.List;
+import org.dataminx.schemas.dts._2009._05.dts.DataTransferType;
+import org.dataminx.schemas.dts._2009._05.dts.JobDefinitionType;
+import org.dataminx.schemas.dts._2009._05.dts.JobDescriptionType;
+import org.dataminx.schemas.dts._2009._05.dts.SubmitJobRequest;
+import org.springframework.util.Assert;
+
+/**
+ * Collection of convenience methods for dealing with DTS input data structures.
+ *
+ * @author Alex Arana
+ */
+public final class InputDataUtils {
+    /**
+     * Extracts any {@link DataTransferType} elements contained in the input {@link SubmitJobRequest}
+     * data structure.
+     * <p>
+     * It is worth noting that it is <em>not</em> the purpose of this method to "hide" genuine errors
+     * that may occur in the process of extracting the underlying data (eg. NPE's etc) but merely to
+     * allow application code to provide more meaningful errors in due course...
+     *
+     * TODO consider removing this method later on as it goes against one of my most sacred development
+     *      principles: failing fast
+     *
+     * @param submitJobRequest Object corresponding to an DTS job request
+     * @return A list holding instances of {@link DataTransferType} elements contained in the input
+     *         DTS job request.  This method can return <code>null</code> in some circumstances
+     */
+    public static List<DataTransferType> getDataTransfers(final SubmitJobRequest submitJobRequest) {
+        Assert.notNull(submitJobRequest);
+        List<DataTransferType> result = null;
+        final JobDefinitionType jobDefinition = submitJobRequest.getJobDefinition();
+        if (jobDefinition != null) {
+            final JobDescriptionType jobDescription = jobDefinition.getJobDescription();
+            if (jobDescription != null) {
+                result = jobDescription.getDataTransfer();
+            }
+        }
+        return result;
+    }
+}
