52a51ab8c6abaa3af7ff3d6aed8b8b2ef19134d3
gerson721 <gerson721@89d2972e-3acd-11de-8ef0-0f53c1b7e458>
2009-08-25 03:55:16 +0000

    branch out so my changes wouldn't cause any conflict with changes done on dts-workernode in trunk.

--- dts-portal/src/main/java/org/dataminx/dts/portal/DtsAction.java
+++
@@ -1,8 +0,0 @@
-package org.dataminx.dts.portal;
-
-public interface DtsAction {
-
-    static String SOAP_FAULT_CLIENT_ERROR = "soapFaultClientError";
-    static String WEB_SERVICE_IO_ERROR = "webServiceIOError";
-
-}
