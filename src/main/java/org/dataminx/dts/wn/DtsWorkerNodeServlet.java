95086de264d8fc65e83738b9c2ce46396d075e42
gerson721 <gerson721@89d2972e-3acd-11de-8ef0-0f53c1b7e458>
2009-09-01 00:45:31 +0000

    - provided an option of deploying the dts-workernode as a webapp - fixed issues with security libraries having to be shared when the dts-ws and dts-workernode modules are deployed on the same web app container.

---
+++ src/main/java/org/dataminx/dts/wn/DtsWorkerNodeServlet.java
@@ -0,0 +1,40 @@
+package org.dataminx.dts.wn;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * The Servlet that will start the DtsWorkerNode service.
+ *
+ * @author Gerson Galang
+ */
+public class DtsWorkerNodeServlet extends HttpServlet {
+
+    /** The logger. */
+    private static final Log LOGGER = LogFactory.getLog(DtsWorkerNodeServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        super.doGet(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+
+    public void init(ServletConfig servletConfig) throws ServletException {
+        super.init(servletConfig);
+        String dataminxDirectory = servletConfig.getInitParameter("dataminxDirectory");
+
+        if (true) {
+            new DtsWorkerNodeCommandLineRunner(dataminxDirectory).run();
+        }
+    }
+}
