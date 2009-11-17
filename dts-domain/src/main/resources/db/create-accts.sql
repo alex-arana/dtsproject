7529ad9cd39684b059516a30b6ed7eec46c0edae
gerson721 <gerson721@89d2972e-3acd-11de-8ef0-0f53c1b7e458>
2009-11-17 03:13:56 +0000

    removed dts-domain as the classes in this module have been moved to dts-ws

--- dts-domain/src/main/resources/db/create-accts.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE dts;
-CREATE USER 'dts'@'localhost' IDENTIFIED BY 'dts';
-GRANT ALL PRIVILEGES ON dts.* TO 'dts'@'localhost';
-GRANT ALL PRIVILEGES ON dts.* TO 'dts'@'%';
-GRANT ALL PRIVILEGES ON dts.* TO 'dts'@'127.0.0.1';
