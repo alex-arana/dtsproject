7751e600ccf3a435efd135d91b2bc712148714be
gerson721 <gerson721@89d2972e-3acd-11de-8ef0-0f53c1b7e458>
2010-02-05 03:08:10 +0000

    added getJobDetails WS method

--- dts-wsclient/src/main/java/org/dataminx/dts/client/sws/DataTransferServiceClient.java
+++ dts-wsclient/src/main/java/org/dataminx/dts/client/sws/DataTransferServiceClient.java
@@ -27,59 +27,72 @@
  */
 package org.dataminx.dts.client.sws;
 
+import org.dataminx.schemas.dts.x2009.x07.messages.JobDetailsType;
 import org.ggf.schemas.jsdl.x2005.x11.jsdl.JobDefinitionDocument;
 import org.springframework.ws.client.core.WebServiceMessageCallback;
 
 /**
  * The Data Transfer Service WS Client.
- *
+ * 
  * @author Gerson Galang
  */
 public interface DataTransferServiceClient {
 
     /**
      * Submit the DTS Job.
-     *
+     * 
      * @param dtsJob the DTS Job definition
-     * @return the Universally Unique Identifier for the submitted job or null if an error occurred during the job
-     * submission
+     * @return the Universally Unique Identifier for the submitted job or null
+     *         if an error occurred during the job submission
      */
     String submitJob(JobDefinitionDocument dtsJob);
 
     /**
      * Cancel the job.
-     *
-     * @param jobResourceKey the Universally Unique Identifier (aka Global Unique Identifier) of the job to cancel
+     * 
+     * @param jobResourceKey the Universally Unique Identifier (aka Global
+     *        Unique Identifier) of the job to cancel
      */
     void cancelJob(String jobResourceKey);
 
     /**
      * Suspend the job.
-     *
-     * @param jobResourceKey the Universally Unique Identifier (aka Global Unique Identifier) of the job to suspend
+     * 
+     * @param jobResourceKey the Universally Unique Identifier (aka Global
+     *        Unique Identifier) of the job to suspend
      */
     void suspendJob(String jobResourceKey);
 
     /**
      * Resume the job.
-     *
-     * @param jobResourceKey the Universally Unique Identifier (aka Global Unique Identifier) of the job to resume
+     * 
+     * @param jobResourceKey the Universally Unique Identifier (aka Global
+     *        Unique Identifier) of the job to resume
      */
     void resumeJob(String jobResourceKey);
 
     /**
      * Gets the job status.
-     *
-     * @param jobResourceKey the Universally Unique Identifier (aka Global Unique Identifier) of the job being
-     * queried about
+     * 
+     * @param jobResourceKey the Universally Unique Identifier (aka Global
+     *        Unique Identifier) of the job being queried about
      * @return the job status
      */
     String getJobStatus(String jobResourceKey);
 
+    /**
+     * Get the job details.
+     * 
+     * @param jobResourceKeythe Universally Unique Identifier (aka Global Unique
+     *        Identifier) of the job being queried about
+     * @return the job details
+     */
+    JobDetailsType getJobDetails(String jobResourceKey);
 
     /**
-     * Sets the web service message callback if further processing is required on the web service message.
-     *
+     * Sets the web service message callback if further processing is required
+     * on the web service message.
+     * 
      * @param wsMessageCallback the web service message callback
      */
     void setWebServiceMessageCallback(WebServiceMessageCallback wsMessageCallback);
