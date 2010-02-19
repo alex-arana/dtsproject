48459e51c4061b4019dd0d0a59b477322405c09e
hungusyd <hungusyd@89d2972e-3acd-11de-8ef0-0f53c1b7e458>
2010-02-19 01:38:54 +0000

    rename to DtsJobScheduler

---
+++ trunk/dts-broker/src/main/java/org/dataminx/dts/broker/si/DtsJobScheduler.java
@@ -0,0 +1,90 @@
+/**
+ *
+ */
+package org.dataminx.dts.broker.si;
+
+import java.util.Calendar;
+import java.util.Date;
+import javax.xml.bind.DatatypeConverter;
+import org.apache.xmlbeans.XmlException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.core.Message;
+import org.springframework.integration.handler.DelayHandler;
+import org.springframework.integration.message.MessageBuilder;
+import org.springframework.integration.xml.transformer.XmlPayloadUnmarshallingTransformer;
+
+
+/**
+ * An {@link Handler} that processes the incoming message and output an message that can be
+ * scheduled by the {@link DelayHandler}. Other implementation can do the scheduling by itself
+ * instead of relying on {@link DelayHandler}.
+ *
+ * @author hnguyen
+ */
+@MessageEndpoint("brokerJobSubmissionChannel")
+public class DtsJobScheduler {
+    /** A reference to the internal logger object. */
+    private static final Logger LOG = LoggerFactory.getLogger(DtsJobScheduler.class);
+
+    /** Extracting delay information from the message payload object*/
+    @Autowired
+    @Qualifier("xmlPayloadDelayExtractor")
+    private DelayExtractor extractor;
+
+    @Autowired
+    @Qualifier("dtsMessagePayloadTransformer")
+    private XmlPayloadUnmarshallingTransformer transformer;
+
+    public String getDelayHeaderName() {
+        return delayHeaderName;
+    }
+
+    @Required
+    public void setDelayHeaderName(String delayHeaderName) {
+        this.delayHeaderName = delayHeaderName;
+    }
+
+    /** delay header that will store the delay information for {@link DelayHandler} to process */
+    private String delayHeaderName;
+
+    /**
+     * Schedules the message according to the info in the message payload. If
+     * no info is present, schedules the message to be sent immediately.
+     *
+     * @param message DTS Job message to be schedules
+     * @throws XmlException
+     */
+    @ServiceActivator
+    public Message<?> schedule(Message<?> message){
+
+        LOG.debug(message.getPayload().toString());
+
+        Message<?> xmlMsg = transformer.transform(message);
+        String delay = extractor.extractDelay(xmlMsg.getPayload());
+
+        if (LOG.isDebugEnabled()){
+            LOG.debug("Found delay transfer requirement: " + delay);
+        }
+        try {
+            Calendar cal = DatatypeConverter.parseDateTime(delay);
+            Date tillDate = cal.getTime();
+            return buildMessageWithNewHeader(message, tillDate);
+        }
+        catch (IllegalArgumentException e) {
+            LOG.warn("Delay is not xsd:datetime formatted");
+            return buildMessageWithNewHeader(message, delay);
+        }
+   }
+
+    /* Constructs a new message with approriate header name/value for {@link DelayHandler} to process */
+    private Message<?> buildMessageWithNewHeader(Message<?> message, Object headerValue) {
+        return MessageBuilder.fromMessage(message).setHeader(delayHeaderName, headerValue).build();
+    }
+
+}
