ac46c4dc4c33d84d97833047cc1163dbe9fab9d3
gerson721 <gerson721@89d2972e-3acd-11de-8ef0-0f53c1b7e458>
2010-03-31 00:56:48 +0000

    refactored and made a couple of changes to the dummy implementation of the Encrypter so that the algorithm is included in the encrypted message.

---
+++ trunk/dts-security/src/main/java/org/dataminx/dts/security/crypto/CryptoLoader.java
@@ -0,0 +1,7 @@
+package org.dataminx.dts.security.crypto;
+
+public interface CryptoLoader {
+
+    Encrypter getEncrypter();
+
+}
--- trunk/dts-security/src/main/java/org/dataminx/dts/security/crypto/DummyEncrypter.java
+++ trunk/dts-security/src/main/java/org/dataminx/dts/security/crypto/DummyEncrypter.java
@@ -1,13 +1,13 @@
-package org.dataminx.dts.security.crypt;
+package org.dataminx.dts.security.crypto;
 
 class DummyEncrypter implements Encrypter {
 
     public String decrypt(final String stringToDecrypt) {
-        return stringToDecrypt;
+        return stringToDecrypt.substring(stringToDecrypt.indexOf(":") + 1);
     }
 
     public String encrypt(final String stringToEncrypt) {
-        return stringToEncrypt;
+        return CLEAR_TEXT_HASH + ":" + stringToEncrypt;
     }
 
     public void setPassphrase(final String passphrase) {
